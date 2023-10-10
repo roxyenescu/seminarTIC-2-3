e7878f665bd8561954527d0b82ec6d8fa6ce376c
roxyenescu <115460507+roxyenescu@users.noreply.github.com>
2023-10-10 20:03:04 +0300

    calculator css

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 console.log("access granted")
+
+// const button = document.querySelector('#doSomething')
+
+// button.addEventListener("click", printButtonText)
+
+// function printButtonText(e) {
+//     console.log(e.target.innerText)
+// }
+
